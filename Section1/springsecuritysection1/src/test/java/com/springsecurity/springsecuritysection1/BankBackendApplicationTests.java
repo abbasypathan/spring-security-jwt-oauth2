67496da946da2b6d72a184004f0bb7507cd779d5
Abbas <abbaspathan313@gmail.com>
2026-05-20 23:14:15 +0530

    Spring Security Internal Flow

---
+++ Section1/springsecuritysection1/src/test/java/com/springsecurity/springsecuritysection1/BankBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springsecurity.springsecuritysection1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
