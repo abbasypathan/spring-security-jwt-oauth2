7d03c4b089af40f42a8ef311f30677d66214c1be
Abbas <abbaspathan313@gmail.com>
2026-06-19 22:45:56 +0530

    JWT generated & validated

---
+++ Section_11/springsecuritysection11/src/main/java/com/springsecurity/constant/Constants.java
@@ -0,0 +1,7 @@
+package com.springsecurity.constant;
+
+public final class Constants {
+    public static final String JWT_SECRET_DEFAULT_VALUE = "wwre526wiugd38eydeh3hdhh38uh";
+    public static final String JWT_SECRET_KEY = "JWT_SECRET";
+    public static final String JWT_TOKEN_HEADER = "Authorization";
+}
--- Section_11/springsecuritysection11/src/main/java/com/springsecurity/filter/JWTTokenGeneratorFilter.java
+++ Section_11/springsecuritysection11/src/main/java/com/springsecurity/filter/JWTTokenGeneratorFilter.java
@@ -1,12 +1,22 @@
 package com.springsecurity.filter;
 
+import com.springsecurity.constant.Constants;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.Keys;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.core.env.Environment;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.filter.OncePerRequestFilter;
 
+import javax.crypto.SecretKey;
 import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.util.Date;
+import java.util.stream.Collectors;
 
 public class JWTTokenGeneratorFilter extends OncePerRequestFilter {
     @Override
@@ -15,7 +25,25 @@ public class JWTTokenGeneratorFilter extends OncePerRequestFilter {
         // For example, we can generate a JWT token using the JJWT library and set it in the response header
         // String jwtToken = Jwts.builder().setSubject("user").signWith(SignatureAlgorithm.HS256, "secret").compact();
         // response.setHeader("Authorization", "Bearer " + jwtToken);
-
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        if (null != authentication) {
+            Environment env = getEnvironment();
+            if (null != env) {
+                String secret = env.getProperty(Constants.JWT_SECRET_KEY, Constants.JWT_SECRET_DEFAULT_VALUE);
+                SecretKey secretKey = Keys.hmacShaKeyFor(secret.getBytes(StandardCharsets.UTF_8));
+                String token = Jwts.builder()
+                        .issuer("Eazy Bank")
+                        .setSubject("JWT Token")
+                        .claim("username", authentication.getName())
+                        .claim("authorities", authentication.getAuthorities().stream()
+                                .map(grantedAuthority -> grantedAuthority.getAuthority()).collect(Collectors.joining()))
+                        .issuedAt(new Date())
+                        .expiration(new Date((new Date()).getTime() + 3000000)) // 50 minutes
+                        .signWith(secretKey)
+                        .compact();
+                response.setHeader(Constants.JWT_TOKEN_HEADER, token);
+            }
+        }
         filterChain.doFilter(request, response);
     }
 
